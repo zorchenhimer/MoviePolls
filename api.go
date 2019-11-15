730130e7440274559ec784cf2e16fe1ed87d13ca
Zorchenhimer <zorchenhimer@gmail.com>
2019-11-15 17:33:20 -0500

    Run gofmt on api.go

--- api.go
+++ api.go
@@ -1,11 +1,11 @@
 package moviepoll
 
 import (
-    "net/http"
+	"net/http"
 )
 
-type apiHandler struct {}
+type apiHandler struct{}
 
 func (apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    http.Error(w, "API isn't implemented", http.StatusNotImplemented)
+	http.Error(w, "API isn't implemented", http.StatusNotImplemented)
 }
