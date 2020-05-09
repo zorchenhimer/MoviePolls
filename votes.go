cb5c1b50d3ff277627d6c5fc620ae5780dac44ed
Zorchenhimer <zorchenhimer@gmail.com>
2020-05-09 15:02:12 -0400

    Don't allow a user to vote if they don't have any left
    If a user has already voted for the maximum number of allowable movies,
    don't let them vote for anything more.  Removing a vote is still
    possible.
    
    Fixes #4

--- votes.go
+++ votes.go
@@ -57,6 +57,38 @@ func (s *Server) handlerVote(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	} else {
+
+		// TODO: implement this on the data layer
+		votedMovies, err := s.data.GetUserVotes(user.Id)
+		if err != nil {
+			s.doError(
+				http.StatusBadRequest,
+				fmt.Sprintf("Cannot get user votes: %v", err),
+				w, r)
+			return
+		}
+
+		count := 0
+		for _, movie := range votedMovies {
+			// Only count active movies
+			if movie.Watched == nil && movie.Removed == false {
+				count++
+			}
+		}
+
+		maxVotes, err := s.data.GetCfgInt("MaxUserVotes", DefaultMaxUserVotes)
+		if err != nil {
+			fmt.Printf("Error getting MaxUserVotes config setting: %v\n", err)
+			maxVotes = DefaultMaxUserVotes
+		}
+
+		if count >= maxVotes {
+			s.doError(http.StatusBadRequest,
+				"You don't have any more available votes!",
+				w, r)
+			return
+		}
+
 		if err := s.data.AddVote(user.Id, movieId); err != nil {
 			s.doError(http.StatusBadRequest, "Something went wrong :c", w, r)
 			fmt.Printf("Unable to cast vote: %v\n", err)
