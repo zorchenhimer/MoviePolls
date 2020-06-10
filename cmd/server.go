36550a575d05fc573f2d9bac03564a843102bc0e
Zorchenhimer <zorchenhimer@gmail.com>
2020-06-10 19:43:13 -0400

    Add command line options for the new logger

--- cmd/server.go
+++ cmd/server.go
@@ -6,16 +6,19 @@ import (
 	"os"
 
 	"github.com/zorchenhimer/MoviePolls"
+	"github.com/zorchenhimer/MoviePolls/common"
 )
 
 func main() {
 	var logFile string
+	var logLevel string
 	var debug bool
-	flag.StringVar(&logFile, "log", "", "File to write logs")
+	flag.StringVar(&logFile, "logfile", "", "File to write logs")
+	flag.StringVar(&logLevel, "loglevel", "debug", "Log verbosity")
 	flag.BoolVar(&debug, "debug", false, "Enable debug code")
 	flag.Parse()
 
-	s, err := moviepoll.NewServer(moviepoll.Options{Debug: debug, LogLevel: "debug", LogFile: logFile})
+	s, err := moviepoll.NewServer(moviepoll.Options{Debug: debug, LogLevel: common.LogLevel(logLevel), LogFile: logFile})
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
