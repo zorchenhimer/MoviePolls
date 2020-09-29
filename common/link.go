684d9bdf864328c89721933cbf6330e488156237
CptPie <23438606+CptPie@users.noreply.github.com>
2020-09-30 01:37:21 +0200

    Added new function ValidateLink and DetermineLinkType rework
    ValidateLink prepends the https protocol "hint" when no protocol
    seperator "//" is found in the struct on which the function is called.
    This function was necessary since i.e. "foobar.com" in the link input
    resulted on a functioning link that linked to localhost:8090/foobar.com
    when clicked on the frontend.
    
    I also reworked the DetermineLinkType function to not return the type as
    string and instead set it as "sideeffect" -> works more like a setter
    now.

--- common/link.go
+++ common/link.go
@@ -16,13 +16,24 @@ func (l Link) String() string {
 	return fmt.Sprintf("Link{Id: %v Url: %s Type: %s IsSource: %v}", l.Id, l.Url, l.Type, l.IsSource)
 }
 
-func (l Link) DetermineLinkType() (string, error) {
+func (l *Link) ValidateLink() error {
+	url := l.Url
+	if !strings.Contains(url, "//") {
+		l.Url = "https://" + url
+	}
+	return nil
+}
+
+func (l *Link) DetermineLinkType() error {
 	if strings.Contains(l.Url, "imdb") {
-		return "IMDb", nil
+		l.Type = "IMDb"
+		return nil
 	}
 	if strings.Contains(l.Url, "myanimelist") {
-		return "MyAnimeList", nil
+		l.Type = "MyAnimeList"
+		return nil
 	}
 
-	return "Misc", nil
+	l.Type = "Misc"
+	return nil
 }
