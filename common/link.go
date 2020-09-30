fdcd586913cca141db64e6231d3042db912795da
CptPie <23438606+CptPie@users.noreply.github.com>
2020-09-30 02:18:46 +0200

    Moved the link validation function to the link struct

--- common/link.go
+++ common/link.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -16,12 +17,36 @@ func (l Link) String() string {
 	return fmt.Sprintf("Link{Id: %v Url: %s Type: %s IsSource: %v}", l.Id, l.Url, l.Type, l.IsSource)
 }
 
+var re_validLink = *regexp.MustCompile(`[a-zA-Z0-9:._\+]{1,256}\.[a-zA-Z0-9()]{1,6}[a-zA-Z0-9%_:\+.\/]*`)
+
 func (l *Link) ValidateLink() error {
 	url := l.Url
-	if !strings.Contains(url, "//") {
-		l.Url = "https://" + url
+	if re_validLink.MatchString(url) {
+		url = stripRefFromLink(url)
+
+		if len(url) <= 8 {
+			// lets be stupid when the link is too short
+			if !strings.ContainsAny(url, "//") {
+				l.Url = "https://" + url
+			}
+		} else {
+			// lets be smart when the link is long enough
+			// url[:8] ensures that we only look at the 8 first characters of the url -> where the protocol part should be
+			if !strings.Contains(url[:8], "//") {
+				l.Url = "https://" + url
+			}
+		}
+		return nil
 	}
-	return nil
+	return fmt.Errorf("Invalid link: %v", l.Url)
+}
+
+func stripRefFromLink(link string) string {
+	idx := strings.Index(link, "?ref")
+	if idx != -1 {
+		return link[:idx]
+	}
+	return link
 }
 
 func (l *Link) DetermineLinkType() error {
