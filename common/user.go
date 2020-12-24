571319218ba94ef53d44ce34b683ade62d25f3e6
CptPie <23438606+CptPie@users.noreply.github.com>
2020-12-24 22:21:22 +0100

    Added a new utility function to quickly find the right auth struct

--- common/user.go
+++ common/user.go
@@ -48,6 +48,15 @@ func (u User) IsMod() bool {
 	return u.Privilege >= PRIV_ADMIN
 }
 
+func (u User) GetAuthMethod(method AuthType) (*AuthMethod, error) {
+	for _, auth := range u.AuthMethods {
+		if auth.Type == method {
+			return auth, nil
+		}
+	}
+	return nil, fmt.Errorf("No AuthMethod with type %s found for user %s.", method, u.Name)
+}
+
 func (u User) String() string {
 	return fmt.Sprintf(
 		"User{Id:%d Name:%q Email:%q NotifyCycleEnd:%t NotifyVoteSelection:%t Privilege:%d}",
