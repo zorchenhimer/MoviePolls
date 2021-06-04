6c296cc5836d1f83bfdefe1a90965d7f543f9d01
CptPie <23438606+CptPie@users.noreply.github.com>
2021-06-04 23:07:53 +0200

    Trimming spaces when getting string lengths

--- models/util.go
+++ models/util.go
@@ -88,5 +88,5 @@ func movieContainsTag(movie *Movie, tag string) bool {
 // Returns the length of a string in regards of the "acutal" glypes (i.e. a emoji is counted as
 // one character).
 func GetStringLength(str string) int {
-	return uniseg.GraphemeClusterCount(str)
+	return uniseg.GraphemeClusterCount(strings.TrimSpace(str))
 }
