fe476243f36849958965d92c6e0a6a138f2cd81f
CptPie <23438606+CptPie@users.noreply.github.com>
2021-06-14 01:29:43 +0200

    Moved around some handler functions

--- web/handlerStatic.go
+++ web/handlerStatic.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"path/filepath"
 	"strings"
+
+	"github.com/zorchenhimer/MoviePolls/models"
 )
 
 func (s *webServer) handlerStatic(w http.ResponseWriter, r *http.Request) {
@@ -12,4 +14,26 @@ func (s *webServer) handlerStatic(w http.ResponseWriter, r *http.Request) {
 		s.l.Info("Attempting to serve file %q", file)
 	}
 	http.ServeFile(w, r, file)
+}
+
+func (s *webServer) handlerPosters(w http.ResponseWriter, r *http.Request) {
+	file := strings.TrimLeft(filepath.Clean("/"+r.URL.Path), "/\\")
+	if s.debug {
+		s.l.Info("Attempting to serve file %q", file)
+	}
+
+	// Dirty but might work out fine
+	if file != "/poster" {
+		http.ServeFile(w, r, file)
+	} else {
+		http.Error(w, "Nothing to see here. Go away!", http.StatusForbidden)
+	}
+}
+
+func (s *webServer) handlerFavicon(w http.ResponseWriter, r *http.Request) {
+	if models.FileExists("data/favicon.ico") {
+		http.ServeFile(w, r, "data/favicon.ico")
+	} else {
+		http.NotFound(w, r)
+	}
 }
