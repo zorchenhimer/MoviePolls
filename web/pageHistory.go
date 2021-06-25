f51c5566c78b25da89607974ad2e697341cada60
CptPie <23438606+CptPie@users.noreply.github.com>
2021-06-25 04:17:51 +0200

    Added pageHistory which handles `/history/`

---
+++ web/pageHistory.go
@@ -0,0 +1,33 @@
+package web
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/zorchenhimer/MoviePolls/models"
+)
+
+// List of past cycles
+func (s *webServer) handlerPageHistory(w http.ResponseWriter, r *http.Request) {
+	past, err := s.backend.GetPastCycles(0, 100)
+	if err != nil {
+		s.doError(
+			http.StatusInternalServerError,
+			fmt.Sprintf("something went wrong :C"),
+			w, r)
+		s.l.Error("Unable to get past cycles: ", err)
+		return
+	}
+
+	data := struct {
+		dataPageBase
+		Cycles []*models.Cycle
+	}{
+		dataPageBase: s.newPageBase("Cycle History", w, r),
+		Cycles:       past,
+	}
+
+	if err := s.executeTemplate(w, "history", data); err != nil {
+		s.l.Error("Error rendering template: %v", err)
+	}
+}
