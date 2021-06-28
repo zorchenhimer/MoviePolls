7caf29b3d20c98c36c64be72a80cc54da0fed3a1
CptPie <23438606+CptPie@users.noreply.github.com>
2021-06-28 22:12:57 +0200

    Several minor fixes to make it compile/run

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/zorchenhimer/MoviePolls/database"
 	"github.com/zorchenhimer/MoviePolls/logic"
 	"github.com/zorchenhimer/MoviePolls/models"
 	"github.com/zorchenhimer/MoviePolls/web"
@@ -19,13 +20,18 @@ func main() {
 		os.Exit(1)
 	}
 
+	config := web.Options{
+		Debug:  true,
+		Listen: ":8080",
+	}
+
 	log.Info("Running version: %s", ReleaseVersion)
-	if options.Debug {
-		l.Info("Debug mode turned on")
+	if config.Debug {
+		log.Info("Debug mode turned on")
 	}
 
 	// init database
-	data, err := database.GetDataConnector("json", "db/data.json", log)
+	data, err := database.GetDatabase("json", "db/data.json", log)
 	if err != nil {
 		fmt.Printf("Unable to load json data: %v\n", err)
 		os.Exit(1)
@@ -36,11 +42,6 @@ func main() {
 	if err != nil {
 		fmt.Printf("Unable to load backend: %v\n", err)
 		os.Exit(1)
-	}
-
-	config := web.Options{
-		Debug:  true,
-		Listen: ":8080",
 	}
 
 	// init frontend
